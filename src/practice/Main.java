b043a30fddc0761b13ed4f83c0f9c50d54179a57
rowin1102 <rowin1102@naver.com>
2025-03-28 09:46:59 +0900

    .

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,21 +1,14 @@
 package practice;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int num = 0, max = 0;
-		int[] arr = new int[9];
-		for(int i=0; i<9; i++) {
-			num = Integer.parseInt(br.readLine());
-			arr[i] = num;
-		}
+		
 		
 	}
 	
