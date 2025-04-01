24ea1c3ce923d9f7ae8c2b84cf7fd8561a740de1
rowin1102 <rowin1102@naver.com>
2025-04-01 18:21:48 +0900

    2025-04-01

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,12 +1,34 @@
 package practice;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num1 = Integer.parseInt(br.readLine());
+		int num2 = Integer.parseInt(br.readLine());
+		int num3 = Integer.parseInt(br.readLine());
+		
+		String[] result = Integer.toString(num1 * num2 * num3).split("");
+		int[] arr = new int[10];
+		
+		for(int i=0; i<result.length; i++) {
+			int change = Integer.parseInt(result[i]);
+			for(int j=0; j<10; j++) {
+				if(change == j) {
+					arr[j]++;
+				}
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 		
 	}
 	
