603ced91c9011fe0e4d14cc06b6008d6c738fd4d
rowin1102 <rowin1102@naver.com>
2025-04-02 10:41:43 +0900

    2025-04-02

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -10,25 +10,7 @@ public class Main {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int num1 = Integer.parseInt(br.readLine());
-		int num2 = Integer.parseInt(br.readLine());
-		int num3 = Integer.parseInt(br.readLine());
 		
-		String[] result = Integer.toString(num1 * num2 * num3).split("");
-		int[] arr = new int[10];
-		
-		for(int i=0; i<result.length; i++) {
-			int change = Integer.parseInt(result[i]);
-			for(int j=0; j<10; j++) {
-				if(change == j) {
-					arr[j]++;
-				}
-			}
-		}
-		
-		for(int i=0; i<arr.length; i++) {
-			System.out.println(arr[i]);
-		}
 		
 	}
 	
