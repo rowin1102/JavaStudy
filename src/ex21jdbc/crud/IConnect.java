54f25145d39fe5b4fc95c1dd5d2baa391a995ef8
rowin1102 <rowin1102@naver.com>
2025-04-16 14:36:49 +0900

    .

--- src/ex21jdbc/crud/IConnect.java
+++ src/ex21jdbc/crud/IConnect.java
@@ -9,15 +9,15 @@ public interface IConnect {
 	
 	// 멤버상수 : 오라클드라이버, 커넥션URL 선언
 	String ORACLE_DRIVER = "oracle.jdbc.OracleDriver";
-//	String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+	String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+//	String ORACLE_URL = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
 
 	
 	
 	// 멤버메서드
 	void dbExecute(); // 쿼리문 실행
 	void dbClose(); // DB 자원 반납
-	// 사용자로부터 입력ㅇ르 받기 위해 정의
+	// 사용자로부터 입력르 받기 위해 정의
 	String inputValue(String title); 
 	
 }
